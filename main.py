504b4ced5c496e9a96c3b1017542611abb9dd5ab
Daniel Yadi <yadidaniel02@gmail.com>
2022-08-20 23:32:31 +0100

    Update

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from flask_ckeditor import CKEditor, CKEditorField
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+import email_validator
 
 Base = declarative_base()
 app = Flask(__name__)
